cd1e6ff82776faa9e216b8a1510f439f4e72fce6
NurKevser <nurkevserkadiroglu@gmail.com>
2021-10-09 00:35:13 +0300

    completed weather-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+//action
+import { getWeather } from "./actions/getWeather";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  //set city
+  const [city, setCity] = useState("");
+
+  const weatherSelector = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const getWeatherInfoAction = (city) => dispatch(getWeather(city));
+
+
+  useEffect(() => {
+    getWeatherInfoAction('antalya');
+
+  },[])
+
+  const getWeatherInfo = (e) => {
+    e.preventDefault();
+    if (city === "") {
+    } else {
+      getWeatherInfoAction(city);
+    }
+  };
+
+  let details = "";
+  if (weatherSelector.weatherinfo && weatherSelector.weatherinfo.hasOwnProperty("coord")) {
+    details = 
+      <div className="details">
+        <h4>Weather Details</h4>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {weatherSelector.weatherinfo?.name}-
+          <span>{weatherSelector.weatherinfo?.sys.country}</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p>
+          {Math.floor(weatherSelector.weatherinfo.main?.temp-273.15)}&deg;
+        </p>
+        <p>
+          {weatherSelector.weatherinfo.weather[0]?.description}
+        </p>
+      </div>
+  }else{
+    details = <p>You need to type a city or the city you typed doesn't exist</p>
+  }
+
+  return (
+    <React.Fragment>
+      <div className="App">
+        <header>
+          <h1>Weather App</h1>
+        </header>
+      </div>
+
+      <main>
+        <form onSubmit={getWeatherInfo}>
+          <div className="control">
+            <input
+              type="text"
+              name="name"
+              placeholder="City to check weather, Ex. Paris"
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </div>
+          <input type="submit" value="Check Weather" onClick={getWeatherInfo} />
+        </form>
+        {details}
+      </main>
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// import the Provider
+import { Provider } from "react-redux";
+//import the store
+import Store from './store';
+
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store= {Store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
